08cb0ae05fccafb057cf93a2ff753840bfd3517b
tososomaru <delatornikita@gmail.com>
2021-12-28 14:39:14 +0600

    remove session

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,16 +1,6 @@
 import uvicorn
 
 from src.app.core.settings import settings
-from src.app.db.init_db import init_db
-from src.app.db.session import Session
-
-
-def init() -> None:
-    db = Session()
-    init_db(db)
-
-
-init()
 
 uvicorn.run(
     app='src.app.app:app',
