fa7b117fc62a5287b732f1bf3ebb979169aa5893
tososomaru <delatornikita@gmail.com>
2021-12-28 14:38:24 +0600

    async connect to database

--- src/app/app.py
+++ src/app/app.py
@@ -2,12 +2,15 @@ import json
 
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi_pagination import add_pagination
 from pydantic import ValidationError
-from starlette.responses import JSONResponse
+from fastapi.responses import JSONResponse
 
 from .api.api_v1.api import api_router
 from .api import docs
 from src.app.core.settings import settings
+from .api.api_v1.endpoints import users
+from .db.base import database, Base, engine
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -18,14 +21,29 @@ app = FastAPI(
 @app.exception_handler(RequestValidationError)
 @app.exception_handler(ValidationError)
 async def validation_exception_handler(request, exc):
-    print(f"OMG! The client sent invalid data!: {exc}")
     exc_json = json.loads(exc.json())
-    response = {"message": [], "data": None}
 
-    for error in exc_json:
-        response['message'].append(error['loc'][-1]+f": {error['msg']}")
+    r = {
+        'messages': [error['msg'] for error in exc_json]
+    }
 
-    return JSONResponse(response, status_code=422)
+    return JSONResponse(r, status_code=422)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(docs.router, tags=['docs'])
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(users.router, tags=['auth'])
+
+Base.metadata.create_all(bind=engine)
+
+add_pagination(app)
