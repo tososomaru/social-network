1609e980f1bae323e1432cc25f910c2bdba95d6b
tososomaru <delatornikita@gmail.com>
2021-12-28 14:45:37 +0600

    changed entry point

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI
+import uvicorn
 
-from src.app.api import docs
-from src.app.api.api_v1.api import api_router
 from src.app.core.settings import settings
-from src.app.db.init_db import init_db
-from src.app.db.session import Session
 
-
-def init() -> None:
-    db = Session()
-    init_db(db)
-
-
-init()
-
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+uvicorn.run(
+    app='src.app.app:app',
+    host=settings.SERVER_HOST,
+    port=settings.SERVER_PORT,
+    reload=True,
 )
-
-app.include_router(docs.router, tags=['docs'])
-app.include_router(api_router, prefix=settings.API_V1_STR)
