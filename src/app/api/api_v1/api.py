46a7d0d7c0b4ebfaf5750d6f9e5107ac825db24c
tososomaru <delatornikita@gmail.com>
2021-12-28 14:37:34 +0600

    changed api

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from src.app.api.api_v1.endpoints import auth, posts
+from src.app.api.api_v1.endpoints import posts
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix='/auth', tags=['auth'])
 api_router.include_router(posts.router, prefix='/posts', tags=['posts'])
 
